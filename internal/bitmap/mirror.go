93b02b149bf924701766ae2dfb65f66d098ec5e1
ashpring <artshpringer2006@gmail.com>
2024-09-14 00:48:12 +0500

    some comments

--- internal/bitmap/mirror.go
+++ internal/bitmap/mirror.go
@@ -1,24 +1,28 @@
 package bitmap
 
 func (bmp *BMPFile) MirrorHorizontal() {
+	// Мariable initialization
 	data := bmp.image
 	width := int(bmp.head.Width)
 	height := int(bmp.head.Height)
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width/2; x++ {
+			// Changes the opposite pixels in the row
 			data[y][x], data[y][width-x-1] = data[y][width-x-1], data[y][x]
 		}
 	}
 }
 
 func (bmp *BMPFile) MirrorVertical() {
+	// Мariable initialization
 	data := bmp.image
 	width := int(bmp.head.Width)
 	height := int(bmp.head.Height)
 
 	for y := 0; y < height/2; y++ {
 		for x := 0; x < width; x++ {
+			// Changes the opposite pixels in the column
 			data[y][x], data[height-y-1][x] = data[height-y-1][x], data[y][x]
 		}
 	}
