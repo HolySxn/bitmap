30e28be085f38cf1731d952e6b921a27faa38c1c
akakimbe <kakimbekkst@gmail.com>
2024-09-17 16:45:03 +0500

    final filt

--- main.go
+++ main.go
@@ -1,47 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"bitmap/internal/bitmap"
 )
 
 func main() {
-	//var color string
-	args := os.Args[1:]
-	manage(args)
-	// if readData[1] == "apply" {
-	// 	piece := readData[2]
-	// 	for i := 0; i < len(readData[2]); i++ {
-	// 		if piece[i] == '=' {
-	// 			color = piece[i+1:]
-	// 			piece = piece[:i]
-	// 			break
-	// 		}
-	// 	}
-	// 	if piece == "--filter" {
-	// 		dataPic := readFile("sample.bmp")
-	// 		bmp := bitmap.Decode(dataPic)
-	// 		bmp.Filt(color)
-	// 		dataPic = bitmap.Encode(bmp)
-	// 		createFile(dataPic)
-	// 		os.Exit(0)
-	// 	}
-	// 	// Read file
-	// 	data := readFile("sample.bmp")
+	var color string
+	readData := os.Args[1:]
+	if readData[0] == "apply" {
+		piece := readData[1]
+		for i := 0; i < len(readData[1]); i++ {
+			if piece[i] == '=' {
+				color = piece[i+1:]
+				piece = piece[:i]
+				break
+			}
+		}
+		if piece == "--filter" {
+			dataPic := readFile("sample.bmp")
+			bmp, _ := bitmap.Decode(dataPic)
+			bmp.Filt(color)
+			dataPic, _ = bitmap.Encode(bmp)
+			createFile(dataPic)
+			os.Exit(0)
+		}
+		// Read file
+		data := readFile("sample.bmp")
 
-	// 	// Get BMPFile struct
-	// 	bmp := bitmap.Decode(data)
-	// 	bmp.HeaderInfo()
+		// Get BMPFile struct
+		bmp, _ := bitmap.Decode(data)
+		bmp.HeaderInfo()
 
-	// 	// Some manipulations
-	// 	bmp.Crop()
+		// Some manipulations
+		bmp.Crop()
 
-	// 	// Get array of bytes
-	// 	data = bitmap.Encode(bmp)
+		// Get array of bytes
+		data, _ = bitmap.Encode(bmp)
 
-	// 	// Create new bmp file
-	// 	createFile(data)
-	// } else {
-	// 	fmt.Fprintln(os.Stderr, "Error apply")
-	// 	os.Exit(1)
-	// }
+		// Create new bmp file
+		createFile(data)
+	} else {
+		fmt.Fprintln(os.Stderr, "Error apply")
+		os.Exit(1)
+	}
 }
